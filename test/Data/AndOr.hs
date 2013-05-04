b57678ad17bcce06de02b3350ec3d315a2a73520
andorp <andor.penzes@gmail.com>
2013-05-04 12:43:49 +0200

    Fold and map for And/Or tree

--- test/Data/AndOr.hs
+++ test/Data/AndOr.hs
@@ -1,11 +1,38 @@
 module Data.AndOr where
 
-data AndOr a b
-  = And b [AndOr a b]
-  | Or  b [AndOr a b]
+import Prelude hiding (map)
+import qualified Data.List as L
+import Control.Monad (mapM)
+
+data AndOr a
+  = And  a [AndOr a]
+  | Or   a [AndOr a]
   | Leaf a
   deriving (Show, Eq)
 
-map :: (a -> a') -> (b -> b') -> AndOr a b -> AndOr a' b'
-map = undefined
+map :: (a -> b) -> AndOr a -> AndOr b
+map f (Leaf a)   = (Leaf . f $ a)
+map f (And b ts) = And (f b) (L.map (map f) ts)
+map f (Or  b ts) = Or  (f b) (L.map (map f) ts)
 
+fold :: (a -> c -> b)
+     -> (a -> c -> b)
+     -> (a -> b)
+     -> ([b] -> c)
+     -> ([b] -> c)
+     -> AndOr a -> b
+fold _  _  fl _  _  (Leaf x)   = fl x
+fold fa fo fl ga go (And x ts) = fa x (ga (L.map (fold fa fo fl ga go) ts))
+fold fa fo fl ga go (Or  x ts) = fo x (go (L.map (fold fa fo fl ga go) ts))
+
+foldM
+  :: (Monad m)
+  => (a -> c -> m b)
+  -> (a -> c -> m b)
+  -> (a -> m b)
+  -> ([b] -> m c)
+  -> ([b] -> m c)
+  -> AndOr a -> m b
+foldM _  _  fl _  _  (Leaf x)   = fl x
+foldM fa fo fl ga go (And x ts) = (fa x) =<< ga =<< (mapM (foldM fa fo fl ga go) ts)
+foldM fa fo fl ga go (Or  x ts) = (fo x) =<< go =<< (mapM (foldM fa fo fl ga go) ts)
