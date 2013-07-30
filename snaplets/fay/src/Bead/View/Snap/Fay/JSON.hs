f63caa4a608caf2bbc94930455750e6875116eb9
andorp <andor.penzes@gmail.com>
2013-07-31 01:19:04 +0200

    Removing files

--- snaplets/fay/src/Bead/View/Snap/Fay/JSON.hs
+++
@@ -1,9 +0,0 @@
-module Bead.View.Snap.Fay.JSON where
-
-import FFI
-import Prelude
-import Data.Data
-import Bead.Domain.Shared.Evaulation
-
-toEvResultJSON :: EvResult -> String
-toEvResultJSON = ffi "JSON.stringify(%1)"
