a3dca2574d9d2208b8e6ce4d4d25b2ba959aa90f
andorp <andor.penzes@gmail.com>
2013-11-12 18:19:15 +0100

    CRLF converted to LF before markdown conversation

--- src/Bead/View/Snap/Markdown.hs
+++ src/Bead/View/Snap/Markdown.hs
@@ -1,14 +1,21 @@
-module Bead.View.Snap.Markdown where
+module Bead.View.Snap.Markdown (
+    markdownToHtml
+  ) where
 
 {- A markdown to HTML conversion. -}
 
+import Data.String.Utils (replace)
 import Text.Pandoc.Options (def, writerHtml5)
 import Text.Pandoc.Readers.Markdown (readMarkdown)
 import Text.Pandoc.Writers.HTML (writeHtml)
 
 import Text.Blaze.Html5 (Html)
 
--- Produces an HTML value from the given markdown formatted strings
+-- Produces an HTML value from the given markdown formatted strings what
+-- comes from a text area field, crlf endings must be replaced with lf in the string
 markdownToHtml :: String -> Html
-markdownToHtml = writeHtml def . readMarkdown def
+markdownToHtml = writeHtml def . readMarkdown def . replaceCrlf
+
+replaceCrlf :: String -> String
+replaceCrlf = replace "\r\n" "\n"
 
