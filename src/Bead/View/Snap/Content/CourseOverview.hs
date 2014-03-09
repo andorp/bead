80d27a227901f4f5e5f4c8e854972562f3efacd5
Andor Pénzes <andor.penzes@gmail.com>
2014-03-09 11:50:04 +0000

    CourseOverview: Navigation removed

--- src/Bead/View/Snap/Content/CourseOverview.hs
+++ src/Bead/View/Snap/Content/CourseOverview.hs
@@ -6,11 +6,8 @@ module Bead.View.Snap.Content.CourseOverview (
 import           Data.String (fromString)
 import           Data.Time (UTCTime, getCurrentTime)
 
-import           Text.Blaze.Html5 ((!))
 import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A
 
-import qualified Bead.Controller.Pages as P (Page(..))
 import qualified Bead.Controller.UserStories as Story
 import           Bead.View.Snap.Content
 import           Bead.View.Snap.RequestParams
@@ -50,15 +47,3 @@ courseSubmissionsContent now c s = do
       , "then clicking on the button."
       ]
     H.p . i18n msg $ submissionTable "course-table" now c s
-    H.p $ fromString . msg $ Msg_Home_CourseAdministration_Info $ concat
-      [ "New groups for courses may be created in the Course Settings menu.  Teachers may be also assigned to "
-      , "each of the groups there as well."
-      ]
-    H.p $ i18n msg $ navigation $ [
-        P.CourseAdmin, P.NewTestScript, P.EvaluationTable
-      , P.SetUserPassword, P.UploadFile ]
-
-navigation :: [P.Page] -> IHtml
-navigation links = do
-  msg <- getI18N
-  return $ H.div ! A.id "menu" $ H.ul $ mapM_ (i18n msg . linkToPage) links
