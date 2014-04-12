3975c45265479a1f0d987eb79a9ff9a3f9544621
Andor Pénzes <andor.penzes@gmail.com>
2014-04-12 22:25:13 +0000

    TestAgent daemon is created.

---
+++ src/Bead/Daemon/TestAgentDaemon.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Bead.Daemon.TestAgentDaemon where
+
+import           Control.Exception
+import           Control.Concurrent
+
+import qualified Bead.Controller.Logging as L
+import           Bead.Controller.ServiceContext as S
+import           Bead.Controller.UserStories (runUserStory, testAgentComments)
+import           Bead.View.Snap.Translation (trans)
+
+-- Starts a thread that polls the persistence layer for new test agent comments in every w seconds
+-- and places them into the right place.
+startTestCommentsAgent :: L.Logger -> Int -> Int -> ServiceContext -> IO ()
+startTestCommentsAgent logger initWait wait context = do
+  let agent = do threadDelay (secToMicroSec wait)
+                 ((runUserStory context trans TestAgent testAgentComments) >> return ()) `catch` someException
+                 agent
+  forkIO $ do
+    threadDelay (secToMicroSec initWait)
+    agent
+  return ()
+  where
+    secToMicroSec = (* 1000000)
+
+    someException :: SomeException -> IO ()
+    someException e = do
+      (L.log logger L.ERROR (show e)) `catch` (loggerException e)
+      return ()
+      where
+        loggerException :: SomeException -> SomeException -> IO ()
+        loggerException original e = do
+          print original
+          print e
