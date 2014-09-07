a1462a7f0300232353635735ff2395480c5a54b4
Andor Pénzes <andor.penzes@gmail.com>
2014-09-07 12:19:43 +0000

    Unused imports are removed.

--- src/Bead/View/Snap/Content/Home/Data.hs
+++ src/Bead/View/Snap/Content/Home/Data.hs
@@ -1,25 +1,10 @@
 {-# LANGUAGE OverloadedStrings, CPP #-}
 module Bead.View.Snap.Content.Home.Data where
 
-import           Control.Monad.Identity
-import           Data.Function (on)
-import           Data.List (intersperse, sortBy)
-import qualified Data.Map as Map
-import           Data.Maybe (catMaybes)
-import           Data.String (fromString)
 import           Data.Time
 
-import           Text.Blaze.Html5 ((!))
-import qualified Text.Blaze.Html5 as H
-import qualified Text.Blaze.Html5.Attributes as A (style, id)
-
-import qualified Bead.Controller.Pages as Pages
-import qualified Bead.Controller.UserStories as S
-import           Bead.Domain.Entities as E (Role(..))
-import           Bead.Domain.Evaluation
 import           Bead.View.Snap.Content hiding (userState)
-import qualified Bead.View.UserActions as UA
-import           Bead.View.Snap.Content.SubmissionTableBS as ST
+import           Bead.View.Snap.Content.SubmissionTableBS
 
 data HomePageData = HomePageData {
     userState   :: UserState
