1db962030387936b9b157635c4c4da3013602161
Gabor Pali <pali.gabor@gmail.com>
2015-03-08 17:23:49 +0000

    In markdown, allow translating the embedded LaTeX sections to MathML.

--- src/Bead/View/Markdown.hs
+++ src/Bead/View/Markdown.hs
@@ -5,7 +5,7 @@ module Bead.View.Markdown (
 {- A markdown to HTML conversion. -}
 
 import Data.String.Utils (replace)
-import Text.Pandoc.Options (def)
+import Text.Pandoc.Options
 import Text.Pandoc.Readers.Markdown (readMarkdown)
 import Text.Pandoc.Writers.HTML (writeHtml)
 
@@ -14,7 +14,8 @@ import Text.Blaze.Html5 (Html)
 -- Produces an HTML value from the given markdown formatted strings what
 -- comes from a text area field, crlf endings must be replaced with lf in the string
 markdownToHtml :: String -> Html
-markdownToHtml = writeHtml def . readMarkdown def . replaceCrlf
+markdownToHtml = writeHtml def' . readMarkdown def . replaceCrlf
+  where def' = def { writerHTMLMathMethod = MathML Nothing }
 
 replaceCrlf :: String -> String
 replaceCrlf = replace "\r\n" "\n"
