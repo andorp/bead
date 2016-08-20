9705253e57b404f6dcb56f5e87a147173aec951a
chrislample <clample@testobject.com>
2016-08-20 17:55:20 +0200

    Migrate travis test to use Stack

--- docker/travis-test.sh
+++ docker/travis-test.sh
@@ -2,5 +2,5 @@
 
 /development/init/dev-env-setup.sh
 cd /development/bead
-cabal configure -v2 -f "Tests MySQL LDAP"
-cabal run BeadTest
+stack build --flag Bead:Tests --flag Bead:MySQL --flag Bead:LDAP
+stack exec BeadTest
